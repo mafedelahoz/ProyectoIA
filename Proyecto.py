a256ec7fbf96923d8b03f67163bae6d2ef9e7cb3
mafedelahoz <m.delah@uniandes.edu.co>
2024-04-14 13:23:32 -0500

    Desplazamiento de imagenes

--- Proyecto.py
+++ Proyecto.py
@@ -38,25 +38,23 @@ label_train_path = os.path.join(curr_path,'labels','train')
 label_val_path = os.path.join(curr_path,'labels','val')
 label_test_path = os.path.join(curr_path,'labels','test')
 data_path = ''
-img_path = curr_path+'/images'
+img_path = os.path.join(curr_path,'images')
 label_path = curr_path+'/labels'
-faceList = os.listdir(img_path)  
-
+  
+#Leer las imágenes
+all_files = os.listdir(img_path)
+image_files = [file for file in all_files if os.path.isfile(os.path.join(img_path, file))]
 
 #Dividir el dataset en train, val y test
-dataSize = len(faceList)
+dataSize = len(image_files)
 trainSplit = 0.7
 valSplit = 0.1
 testSplit = 0.2
 
 
-imgTrainList = faceList[:int(dataSize*trainSplit)]
-imgValList = faceList[int(dataSize*trainSplit):int(dataSize*(trainSplit+valSplit))]
-imgTestList = faceList[int(dataSize*(trainSplit+valSplit)):]
-
-imgTrainList = os.listdir(img_train_path)
-imgValList = os.listdir(img_val_path)
-imgTestList = os.listdir(img_test_path)
+imgTrainList = image_files[:int(dataSize*trainSplit)]
+imgValList = image_files[int(dataSize*trainSplit):int(dataSize*(trainSplit+valSplit))]
+imgTestList = image_files[int(dataSize*(trainSplit+valSplit)):]
 
 
 
@@ -80,27 +78,25 @@ def move_files(data_list, source_path, destination_path):
 
 #Función para mover imágenes
 def move_images(data_list, source_path, destination_path):
-    i=0
-    
+    if not os.path.isdir(destination_path):
+        os.makedirs(destination_path)
+        
+    i = 0
     for file in data_list:
-        filepath=os.path.join(source_path, file)
-        dest_path=os.path.join(data_path, destination_path)
-
-        if not os.path.isdir(dest_path):
-            os.makedirs(dest_path)
-        finalimage_path=os.path.join(dest_path, file)
-        img_resized=cv2.resize(cv2.imread(filepath), (def_size, def_size))
-        cv2.imwrite(finalimage_path, img_resized)
-        i=i+1
-    print("Number of files transferred:", i)
-
+        src_file_path = os.path.join(source_path, file)
+        dst_file_path = os.path.join(destination_path, file)
+        
+        # Lee, redimensiona y guarda la imagen
+        img = cv2.imread(src_file_path)
+        img_resized = cv2.resize(img, (def_size, def_size))
+        cv2.imwrite(dst_file_path, img_resized)
+        
+        i += 1
+    
+    print("Número de archivos transferidos:", i)
 def change_extension(file):
     basename=os.path.splitext(file)[0]
     filename=basename+text
     return filename
 
-labelTrainList = list(map(change_extension, imgTrainList))
-labelValList = list(map(change_extension, imgValList))
-labelTestList = list(map(change_extension, imgTestList))
-
-move_images(imgTrainList, img_path, img_train_path)
+move_images(imgTestList, img_path, img_test_path)
