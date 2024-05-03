6176a7aa83621eb349dfeef7a5bb0d5609ac0eb4
mafedelahoz <m.delah@uniandes.edu.co>
2024-05-03 18:19:57 -0500

    Ignore large video file

--- Proyecto.py
+++ Proyecto.py
@@ -1,7 +1,6 @@
 import numpy as np 
 import pandas as pd 
 import os
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import tensorflow as tf
 import random
 import matplotlib.pyplot as plt
